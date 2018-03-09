81ba9c863da60de3cb91763b378bec419115da00
Don Hannema <don.hannema@gmail.com>
2018-03-09 20:06:17 +0100

    moved wordpress to other repository

--- wordpress.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wget --mirror --convert-links --adjust-extension --page-requisites --no-parent --no-check-certificate --cut-dirs=0 --no-host-directories -P WordPress https://senseandsensitivity.nl
